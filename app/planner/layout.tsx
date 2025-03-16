ddad18e3921c721bac3f990190a7959ae245d25a
googoocone1 <googoocone1@naver.com>
2025-03-16 22:02:00 +0900

    planner home y scroll fixed

--- app/planner/layout.tsx
+++ app/planner/layout.tsx
@@ -5,19 +5,59 @@ import { useState } from "react";
 import { IoMenu } from "react-icons/io5";
 
 const navMenu = [
-  { name: "홈", icon: '/planner/navigation/home.png', path: "/planner" },
-  {name : "로드맵", icon: '/planner/navigation/roadmap.png', path: "/planner/roadmap"},
-  {name : "가이드북", icon: '/planner/navigation/guidebook.png', path: "/planner/guidebook"},
-  {name : "스케쥴러", icon: '/planner/navigation/scheduler.png', path: "/planner/scheduler"},
-  {name : "웨딩홀", icon: '/planner/navigation/wedding-hall.png', path: "/planner/wedding-hall"},
-  {name : "스튜디오", icon: '/planner/navigation/studio.png', path: "/planner/studio"},
-  {name :"드레스" , icon: '/planner/navigation/wedding-dress.png', path: "/planner/dress"},
-  {name : "메이크업", icon: '/planner/navigation/makeup.png', path: "/planner/makeup"},
-  {name : "반지/예물", icon: '/planner/navigation/ring.png', path: "/planner/ring"},
-  {name : "가전/가구", icon: '/planner/navigation/furniture.png', path: "/planner/furniture"},
+  { name: "홈", icon: "/planner/navigation/home.png", path: "/planner" },
+  {
+    name: "로드맵",
+    icon: "/planner/navigation/roadmap.png",
+    path: "/planner/roadmap",
+  },
+  {
+    name: "가이드북",
+    icon: "/planner/navigation/guidebook.png",
+    path: "/planner/guidebook",
+  },
+  {
+    name: "스케쥴러",
+    icon: "/planner/navigation/scheduler.png",
+    path: "/planner/scheduler",
+  },
+  {
+    name: "웨딩홀",
+    icon: "/planner/navigation/wedding-hall.png",
+    path: "/planner/wedding-hall",
+  },
+  {
+    name: "스튜디오",
+    icon: "/planner/navigation/studio.png",
+    path: "/planner/studio",
+  },
+  {
+    name: "드레스",
+    icon: "/planner/navigation/wedding-dress.png",
+    path: "/planner/dress",
+  },
+  {
+    name: "메이크업",
+    icon: "/planner/navigation/makeup.png",
+    path: "/planner/makeup",
+  },
+  {
+    name: "반지/예물",
+    icon: "/planner/navigation/ring.png",
+    path: "/planner/ring",
+  },
+  {
+    name: "가전/가구",
+    icon: "/planner/navigation/furniture.png",
+    path: "/planner/furniture",
+  },
 ];
 
-export default function PlannerLayout({ children }: { children: React.ReactNode }) {
+export default function PlannerLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   const handleCloseNav = () => {
@@ -25,44 +65,103 @@ export default function PlannerLayout({ children }: { children: React.ReactNode 
   };
 
   return (
-    <div className="w-full mt-[80px] flex items-center justify-start relative">
+    <div className="w-full flex items-start justify-start relative min-h-screen">
       {/* 모바일 메뉴 버튼 */}
-      <button 
+      <button
         onClick={() => setIsNavOpen(!isNavOpen)}
-        className={`lg:hidden fixed left-0 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-r-lg shadow-md z-50 ${isNavOpen ? 'hidden' : 'block'}`}
+        className={`lg:hidden fixed left-0 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-r-lg shadow-md z-50 ${
+          isNavOpen ? "hidden" : "block"
+        }`}
       >
         <IoMenu size={24} />
       </button>
 
       {/* 네비게이션 */}
-      <div className={`fixed left-0 top-0 w-[290px] h-screen bg-[#f2f4f6] text-[16px] shadow-md flex flex-col px-5 items-center justify-start gap-1 transition-transform duration-300 ease-in-out z-40
-        ${isNavOpen ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0 lg:relative`}>
-        <div className="w-full h-[70px]"></div>
-        <button className="w-full sm:hidden flex items-center justify-end" onClick={handleCloseNav}>X</button>
-        <Menu name="홈" icon="/planner/navigation/home.png" path="/planner" onMenuClick={handleCloseNav}></Menu>
-        <div className="w-full my-3 pl-2 text-left text-[#969696] font-semibold">웨딩 준비</div>
-        <Menu name="로드맵" icon="/planner/navigation/roadmap.png" path="/planner/roadmap" onMenuClick={handleCloseNav}></Menu>
-        <Menu name="가이드북" icon="/planner/navigation/guidebook.png" path="/planner/guidebook" onMenuClick={handleCloseNav}></Menu>
-        <div className="w-full my-3 pl-2  text-left text-[#969696] font-semibold">웨딩 플레너</div>
-        <Menu name="스케쥴러" icon="/planner/navigation/scheduler.png" path="/planner/scheduler" onMenuClick={handleCloseNav}></Menu>
-        <Menu name="웨딩홀" icon="/planner/navigation/wedding-hall.png" path="/planner/wedding-hall" onMenuClick={handleCloseNav}></Menu>
-        <Menu name="스튜디오" icon="/planner/navigation/studio.png" path="/planner/studio" onMenuClick={handleCloseNav}></Menu>
-        <Menu name="드레스" icon="/planner/navigation/wedding-dress.png" path="/planner/dress" onMenuClick={handleCloseNav}></Menu>
-        <Menu name="메이크업" icon="/planner/navigation/makeup.png" path="/planner/makeup" onMenuClick={handleCloseNav}></Menu>
-        <Menu name="가전/가구" icon="/planner/navigation/furniture.png" path="/planner/furniture" onMenuClick={handleCloseNav}></Menu>
+      <div
+        className={`fixed left-0 top-0 w-[290px] h-screen bg-[#f2f4f6] text-[16px] shadow-md flex flex-col px-5 items-center justify-start gap-1 transition-transform duration-300 ease-in-out z-40 
+        ${isNavOpen ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0`}
+      >
+        <div className="w-full h-[170px]"></div>
+        <button
+          className="w-full sm:hidden flex items-center justify-end"
+          onClick={handleCloseNav}
+        >
+          X
+        </button>
+        <Menu
+          name="홈"
+          icon="/planner/navigation/home.png"
+          path="/planner"
+          onMenuClick={handleCloseNav}
+        />
+        <div className="w-full my-3 pl-2 text-left text-[#969696] font-semibold">
+          웨딩 준비
+        </div>
+        <Menu
+          name="로드맵"
+          icon="/planner/navigation/roadmap.png"
+          path="/planner/roadmap"
+          onMenuClick={handleCloseNav}
+        />
+        <Menu
+          name="가이드북"
+          icon="/planner/navigation/guidebook.png"
+          path="/planner/guidebook"
+          onMenuClick={handleCloseNav}
+        />
+        <div className="w-full my-3 pl-2 text-left text-[#969696] font-semibold">
+          웨딩 플레너
+        </div>
+        <Menu
+          name="스케쥴러"
+          icon="/planner/navigation/scheduler.png"
+          path="/planner/scheduler"
+          onMenuClick={handleCloseNav}
+        />
+        <Menu
+          name="웨딩홀"
+          icon="/planner/navigation/wedding-hall.png"
+          path="/planner/wedding-hall"
+          onMenuClick={handleCloseNav}
+        />
+        <Menu
+          name="스튜디오"
+          icon="/planner/navigation/studio.png"
+          path="/planner/studio"
+          onMenuClick={handleCloseNav}
+        />
+        <Menu
+          name="드레스"
+          icon="/planner/navigation/wedding-dress.png"
+          path="/planner/dress"
+          onMenuClick={handleCloseNav}
+        />
+        <Menu
+          name="메이크업"
+          icon="/planner/navigation/makeup.png"
+          path="/planner/makeup"
+          onMenuClick={handleCloseNav}
+        />
+        <Menu
+          name="가전/가구"
+          icon="/planner/navigation/furniture.png"
+          path="/planner/furniture"
+          onMenuClick={handleCloseNav}
+        />
       </div>
 
       {/* 메인 컨텐츠 */}
-      <div className="w-full h-[100vh] ">{children}</div>
+      <div className="w-full  mt-[100px] px-[100px] overflow-y-auto lg:ml-[290px]">
+        {children}
+      </div>
 
       {/* 오버레이 */}
       {isNavOpen && (
-        <div 
+        <div
           className="fixed inset-0 bg-black bg-opacity-50 z-30 lg:hidden"
           onClick={handleCloseNav}
-        ></div>
+        />
       )}
     </div>
   );
 }
-
--- app/planner/page.tsx
+++ app/planner/page.tsx
@@ -7,10 +7,18 @@ export default function Planner() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <div className="w-full mt-20 flex items-center justify-center">
-
+    <div className="w-[1250px]  pb-20 flex  items-center justify-center">
       {/* 메인 컨텐츠 */}
-      <div>HOME</div>
+      <div className="w-full  flex flex-col items-center  justify-center gap-5">
+        <div className="w-full flex flex-col items-center justify-start">
+          <div className="w-full text-[24px] font-semibold">노남주님</div>
+          <div className="w-full text-[24px] font-semibold">안녕하세요!😊</div>
+        </div>
+        <div className="w-full h-[120px] bg-[#ffe3dd] rounded-lg"></div>
+        <div className="w-full h-[540px] bg-gray-200 rounded-lg"></div>
+        <div className="w-full h-[430px] bg-[#ffe3dd] rounded-lg"></div>
+        <div className="w-full h-[140px] bg-gray-200 rounded-lg"></div>
+      </div>
     </div>
   );
 }
